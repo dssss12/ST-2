ca2cc68ba4f696f8d63eaf1887a32188681adb2d
dssss12 <125500790+dssss12@users.noreply.github.com>
2025-03-09 16:29:44 +0300

    tasks.cpp

---
+++ src/tasks.cpp
@@ -0,0 +1,17 @@
+#include "tasks.h"
+#include "circle.h"
+
+double calculateRopeGap() {
+    Circle earth(6378100); // Радиус в метрах
+    earth.setFerence(earth.getFerence() + 1);
+    return earth.getRadius() - 6378100;
+}
+
+double calculatePoolCost() {
+    Circle pool(3);
+    Circle walkway(4);
+    double walkwayArea = walkway.getArea() - pool.getArea();
+    double fenceCost = walkway.getFerence() * 2000;
+    double concreteCost = walkwayArea * 1000;
+    return fenceCost + concreteCost;
+}
